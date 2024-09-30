c0c50841130bc909d25f5b336d2d39a410a1ae52
Sam Tyler <sam.tyler@aligent.com.au>
2024-09-30 16:15:13 +0930

    TF2-9 Set up "self linting"
    (Since we're not actually going to have React as a dependency, overwrite
    the React version in the "dogfood" lint rule).

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,8 @@
 import { eslintConfigs } from './src/index.js';
 
-export default eslintConfigs.react;
+export default [
+    ...eslintConfigs.react,
+    {
+        settings: { react: { version: '18' } },
+    },
+];
