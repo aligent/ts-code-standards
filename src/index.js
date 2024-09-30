b7077d9dae552f70af8fb721a79e8f76516b34a9
Sam Tyler <sam.tyler@aligent.com.au>
2024-09-30 11:11:33 +0930

    TF2-9 Introduce previously discussed rules
    Introduce rules that have been explicitly discussed and agreed upon.

--- src/index.js
+++ src/index.js
@@ -2,7 +2,32 @@ import eslint from '@eslint/js';
 import tsEslint from 'typescript-eslint';
 import eslintPluginPrettier from 'eslint-plugin-prettier/recommended';
 
-const core = [eslint.configs.recommended, ...tsEslint.configs.recommended, eslintPluginPrettier];
+const core = [
+    eslint.configs.recommended,
+    ...tsEslint.configs.recommended,
+    eslintPluginPrettier,
+    {
+        rules: {
+            'no-restricted-syntax': [
+                'error',
+                {
+                    selector: 'TSEnumDeclaration',
+                    message:
+                        'Do not use TypeScript enums. Please use const enums instead. See: https://dev.to/ivanzm123/dont-use-enums-in-typescript-they-are-very-dangerous-57bh for details.',
+                },
+            ],
+            '@typescript-eslint/ban-ts-comment': [
+                'error',
+                {
+                    'ts-expect-error': 'allow-with-description',
+                    'ts-ignore': true,
+                    'ts-nocheck': true,
+                    'ts-check': true,
+                },
+            ],
+        },
+    },
+];
 
 const nextJs = [
     // TODO: TF2-9: Create a config specifically for React/Next
