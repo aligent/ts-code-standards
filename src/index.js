b319c1207c034387daf23ef5b1486d357072830d
Sam Tyler <sam.tyler@aligent.com.au>
2024-10-08 13:21:51 +1030

    TF2-9 Enforce React fragment shorthand
    This means that we always have to use the shorthand unless there's a
    `key`. Hopefully this means we never need to discuss this again.

--- src/index.js
+++ src/index.js
@@ -49,6 +49,7 @@ const react = [
         settings: { react: { version: 'detect' } },
         rules: {
             'react/react-in-jsx-scope': 'off',
+            'react/jsx-fragments': ['error'],
         },
     },
 ];
